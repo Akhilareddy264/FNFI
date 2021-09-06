b8fe2ab2a8c564a1e972346578745df69e23d824
Akhilareddy264 <87795306+Akhilareddy264@users.noreply.github.com>
2021-09-06 17:43:16 +0530

    Add files via upload

---
+++ GFGGFG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleconapp
+{
+    class gfg
+    {
+        public static void Main()
+        {
+            int n = 5;
+            for (int i = 1; i <= 10; ++i)
+                Console.Write(n + " * " + i + " = " + n * i + "\n");
+        }
+    }
+}
